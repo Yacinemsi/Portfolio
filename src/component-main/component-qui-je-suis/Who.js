ae0b7e557e74ab54ff178167a48aa6bcd29b8e93
Moussaoui Yacine <moussaoui.yacine.59@hotmail.com>
2023-10-25 18:25:22 +0200

    intégration du style des different projet en differente version

--- src/component-main/component-qui-je-suis/Who.js
+++ src/component-main/component-qui-je-suis/Who.js
@@ -4,15 +4,15 @@ const Who = () => {
   return (
     <div id="qui-je-suis">
       <div className="bienvenue_item fade-in-bottom">
-        <h1>Bienvenue</h1>
-        <h2>
+        <h1>Développeur front-end React</h1>
+        <p>
           Ici, vous découvrirez l'essence de mon parcours professionnel et de ma
           passion pour le développement. <br></br>Chaque projet reflète une part
           de ma créativité et de mon engagement envers la qualité.<br></br>
           Explorez, inspirez-vous et n'hésitez pas à me contacter pour tout
           échange ou collaboration.
           <br></br>Bonne visite !
-        </h2>
+        </p>
       </div>
     </div>
   );
