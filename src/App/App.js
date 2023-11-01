2d2cb412c17aa6b38fe8a7bfdaa9b6947c161b3f
Moussaoui Yacine <moussaoui.yacine.59@hotmail.com>
2023-11-01 16:20:54 +0100

    Projet finaliser

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,9 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "../Routes/HomePage";
-import NotFoundPage from "../Routes/NotFoundPage";
 
 const App = () => (
   <Routes>
     <Route path="/" element={<HomePage />} />
-    <Route path="*" element={<NotFoundPage />} />
   </Routes>
 );
 
